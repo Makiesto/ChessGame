448d9462ae5a0ffbab70001d04bf93885397cf56
oldboy1506 <wojciech.skwara345@interia.pl>
2025-06-15 20:37:24 +0200

    główna petla gry

---
+++ menu.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+BLUE = (0,0,255)
+
+
+def main_menu(screen)
+
+    font = pygame.font.SysFont("arial", 30)
+    small_font = pygame.font.SysFont("arial",20)
+
+
+
+    while True:
+
+        screen.fill(WHITE)
