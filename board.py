043975ee58c6ad604f39049e5f58d7364403813f
mateusz <makiesto@gmail.com>
2025-06-15 17:44:13 +0200

    add draw method to Board class for rendering the chessboard and pieces also add highlighting recent and available moves

--- board.py
+++ board.py
@@ -1,4 +1,3 @@
-import pygame
 from pieces import *
 
 # Kolory pól i podświetleń ruchów
@@ -6,6 +5,7 @@ WHITE = (255, 255, 255)
 GRAY = (160, 160, 160)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+
 
 class Board:
     """
@@ -20,12 +20,12 @@ class Board:
         self.rows = self.cols = 8
         self.square_size = 640 // self.cols
         self.board = [[None for _ in range(self.cols)] for _ in range(self.rows)]
-        self.turn = 'w' # 'w' - białe, 'b' - czarne -> białe zaczynają
-        self.selected = None # Aktualnie wybrana figura (współrzędna)
-        self.legal_moves = [] # Dozwolone ruchy dla wybranej figury
-        self.last_move = None # Ostatni ruch (do podświetleń)
-        self.winner = None # Zwyciężca: 'Białe', 'Czarne', lub 'Remis'
-        self.en_passant_target = None # Pole oznaczające bicie w przelocie
+        self.turn = 'w'  # 'w' - białe, 'b' - czarne -> białe zaczynają
+        self.selected = None  # Aktualnie wybrana figura (współrzędna)
+        self.legal_moves = []  # Dozwolone ruchy dla wybranej figury
+        self.last_move = None  # Ostatni ruch (do podświetleń)
+        self.winner = None  # Zwyciężca: 'Białe', 'Czarne', lub 'Remis'
+        self.en_passant_target = None  # Pole oznaczające bicie w przelocie
         self.place_pieces()
 
     def place_pieces(self):
@@ -38,3 +38,32 @@ class Board:
         for col, piece_class in enumerate(pieces_order):
             self.board[0][col] = piece_class('b')
             self.board[7][col] = piece_class('w')
+
+    def draw(self, win):
+        """
+        Rysowanie sachownicy, figur, możliwych ruchów i ostatniego ruchu.
+        :param win: (pygame.Surface) powierzchnia do rysowania (okno gry)
+        """
+
+        for row in range(self.rows):
+            for col in range(self.cols):
+                # Rsyowanie planszy
+                color = WHITE if (row + col) % 2 == 0 else GRAY
+                pygame.draw.rect(win, color,
+                                 (col * self.square_size, row * self.square_size, self.square_size, self.square_size))
+
+                # Podświetalnie możliwych ruchów
+                if (row, col) in self.legal_moves:
+                    pygame.draw.rect(win, GREEN, (
+                    col * self.square_size, row * self.square_size, self.square_size, self.square_size), 3)
+
+                # Podświetlenie ostatniego ruchu
+                if self.last_move == (row, col):
+                    overlay = pygame.Surface((self.square_size, self.square_size), pygame.SRCALPHA)
+                    pygame.draw.rect(overlay, (255, 0, 0, 120), overlay.get_rect(), 3)
+                    win.blit(overlay, (col * self.square_size, row * self.square_size))
+
+                # Rysowanie figury
+                figura = self.board[row][col]
+                if figura:
+                    figura.draw(win, row, col, self.square_size)
