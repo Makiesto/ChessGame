201944d826aa5911282c83f1ee919501714fd72d
oldboy1506 <wojciech.skwara345@interia.pl>
2025-06-15 22:38:57 +0200

    główna pętla gry

--- game.py
+++ game.py
@@ -12,3 +12,28 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
+def run(self):
+
+    while self.running:
+        self.clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                self.running = False
+            elif event.type ==pygame.MOUSEBUTTONDOWN:
+                x,y = pygame.mouse.get_pos()
+                row,col = y // self.board.square_size, x // self.board.square_size
+                self.board.select(row, col)
+
+        self.board.draw(self.screen)
+        pygame.display.update()
+
+
+        if self.board.winner:
+
+            self.display_winner(self.board.winner)
+            save_result(self.board.winner)
+            self.wait_for_postgame_action()
+            self.running =False
+
+
