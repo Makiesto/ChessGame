862af78dcd093f80aa52fdac37c719fc905ff7c2
oldboy1506 <wojciech.skwara345@interia.pl>
2025-06-15 23:08:12 +0200

    wyswietlenie ekrnu końcowego oraz menu po zakończeniu gry

--- game.py
+++ game.py
@@ -36,4 +36,42 @@ def run(self):
             self.wait_for_postgame_action()
             self.running =False
 
+def dsiplay_winner(self,winner):
+
+    font = pygame.font.SysFont("arial",20)
+    if self.board.is_insufficient_material():
+        text = font.render("remis!", True,  (255,255,0))
+    else:
+        text =font.render(f"Szach_Mat! Wygrywa{winner}", True, (255,0,0))
+
+
+    self.screen.fill((0,0,0))
+    self.screen.bilt(text,((640- text.get_width())//2,200))
+
+    button_font = pygame.font.SysFont("arial", 28)
+
+    menu_text = button_font.render("Pwrót do menu", True, (255,255,255))
+    menu_rect = pygame.rect(195,280,250,50)
+    pygame.draw.rect(self.screen, (100,100,100), score_rect)
+    self.screen.blit(score_text, (score_rect.x +20, core_rect.y + 10))
+
+    pygame.display.update()
+
+    self.menu_button = menu_rect
+    self.score_button = score_rect
+
+def wait_for_postgame_action(self):
+    waiting =True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x,y = pygame.mouse.get_pos()
+            if self.menu_buttoncolidepoint(x,y):
+                waiting=False
+            elif self.score_button.colidepoint(x,y):
+                self.display_score_table()
+
+                self.display_winner(self.board.winner)
 
