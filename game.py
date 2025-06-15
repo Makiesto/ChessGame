19abec3beebcdc2f4bfe78bec0db070e84e17a07
oldboy1506 <wojciech.skwara345@interia.pl>
2025-06-16 00:17:40 +0200

    Dodanie komentarzy, małe naprawy błędów

--- game.py
+++ game.py
@@ -4,33 +4,48 @@ from score import save_result, get_score_summary
 
 
 class Game:
-
+    """
+    Klasa reprezentująca logikę gry szachowej wraz z interfejsem graficznym i dźwiękiem.
+    Zarządza przebiegiem gry, obsługuje zdarzenia użytkownika, rysowanie oraz ekran końcowy.
+    """
     def __init__ (self, screen):
+        """
+        Inicjalizacja gry: ustawienie planszy, zegara, dźwięków oraz głównej pętli.
 
+        """
         self.screen = screen
         self.board= Board()
         self.clock = pygame.time.Clock()
         self.running = True
-
+# Inicjalizacja dźwięków gry
         pygame.mixer.init()
         self.move_sound = pygame.mixer.Sound("asssets/sounds/move_sound.wav")
         self.check_sound = pygame.mixer.Sound("asssets/sounds/check_sound.mp3")
-
+    #przechowywanie metody wykonania ruchu, aby dodać efekty dźwiękowe
         self.original_movie_piece = self.board.move_piece
         self.board.movie_piece =self.wrapped_move_piece
 
     def wrapped_move_piece(self,start,end):
+        """
+        Zastępcza metoda wykonująca ruch, dodająca dźwięki ruchu i szacha.
+
+        :param start: współrzędne pola początkowego (rząd, kolumna)
+        :param end: współrzędne pola końcowego (rząd, kolumna)
+
+        """
         current_turn= self.board.turn
         self.original_movie_piece(start,end)
         self.move_sound.play()
+        # Jeśli po ruchu przeciwnik jestw szachu, odtwórz dźwięk
         if self.board.is_check(self.board.turn):
             self.check_sound.play()
 
     def run(self):
-
+# Główna petla gry. Obsługuje zdarzenia, rysuje i sprawdza warunki zakończenia.
         while self.running:
             self.clock.tick(60)
 
+            #Obsługa zdarzeń (zamknięcię okna, kliknięcie myszy)
             for event in pygame.event.get():
                 if event.type ==pygame.QUIT:
                     self.running = False
@@ -38,11 +53,11 @@ class Game:
                     x,y = pygame.mouse.get_pos()
                     row,col = y // self.board.square_size, x // self.board.square_size
                     self.board.select(row, col)
-
+            #Rysowanie planszy i aktualizacja okna
             self.board.draw(self.screen)
             pygame.display.update()
 
-
+            # Sprawdzenie czy gra się zakończyła
             if self.board.winner:
 
                 self.display_winner(self.board.winner)
@@ -50,9 +65,11 @@ class Game:
                 self.wait_for_postgame_action()
                 self.running =False
 
-    def dsiplay_winner(self,winner):
-
-        font = pygame.font.SysFont("arial",20)
+    def display_winner(self,winner):
+""" Wyświetla ekran końcowy z informacją o zwycięzcy lub remisie.
+: param winner: tekst informujący o zwycięzcy ("Białe", "czarne", "remis")
+"""
+        font = pygame.font.SysFont("arial",36)
         if self.board.is_insufficient_material():
             text = font.render("remis!", True,  (255,255,0))
         else:
@@ -62,12 +79,18 @@ class Game:
         self.screen.fill((0,0,0))
         self.screen.bilt(text,((640- text.get_width())//2,200))
 
+        # Rysowanie przyciskó gry
         button_font = pygame.font.SysFont("arial", 28)
 
-        menu_text = button_font.render("Pwrót do menu", True, (255,255,255))
-        menu_rect = pygame.rect(195,280,250,50)
+        menu_text = button_font.render("Powrót do menu", True, (255,255,255))
+        menu_rect = pygame.Rect(195,280,250,50)
+        pygame.draw.rect(self.screen, (100,100,100), menu_rect)
+        self.screen.blit(menu_text, (menu_rect.x + 20, menu_rect.y + 10))
+
+        score_text =button_font.render("Tabela Wyników", True, (255, 255, 255))
+        score_rect = pygame.Rect(195,350,250,50)
         pygame.draw.rect(self.screen, (100,100,100), score_rect)
-        self.screen.blit(score_text, (score_rect.x +20, core_rect.y + 10))
+        self.screen.blit(score_text, (score_rect.x +20, score_rect.y + 10))
 
         pygame.display.update()
 
@@ -75,6 +98,10 @@ class Game:
         self.score_button = score_rect
 
     def wait_for_postgame_action(self):
+        """Oczekuje na wybór użytkownika po zakonczeniu gry
+        - powrót do menu
+        - wyświetlenie tabeli wyników (potem znów powrót do wyboru)
+        """
         waiting =True
         while waiting:
             for event in pygame.event.get():
@@ -83,21 +110,27 @@ class Game:
                     exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     x,y = pygame.mouse.get_pos()
-                    if self.menu_buttoncolidepoint(x,y):
+                    if self.menu_button.collidepoint(x,y):
                         waiting=False
-                    elif self.score_button.colidepoint(x,y):
+                    elif self.score_button.collidepoint(x,y):
                         self.display_score_table()
-
+                        #Po powrocie z tabeli wyników, odśwież ekran końcowy gry
                         self.display_winner(self.board.winner)
-    def dsiplay_score_table(self):
+
+    def display_score_table(self):
+        """
+        Wyświetla tabelę wyników z licznikami zwycięstw białych i czarnych        :param self:
+
+        """
 
         white_wins, black_wins = get_score_summary()
         self.screen.fill((20,20,20))
 
         font= pygame.font.SysFont("arial", 32)
         title = font.render("tabela wyników", True, (255,255,255))
-        self.scren.blit(title, ((640 - title.get_width())//2 , 50 ))
+        self.screen.blit(title, ((640 - title.get_width())//2 , 50 ))
 
+        #Etykiety i wartości punktów
         label_font = pygame.font.SysFont("arial", 26)
         label1 = label_font.render("białe", True, (255,255,255))
         label2 = label_font.render("Carne", True, (255,255,255))
@@ -111,12 +144,14 @@ class Game:
         self.screen.blit(label2, (400,150))
         self.screen.blit(black_text, (420,190))
 
+        #Notka informacyjna
         note_font = pygame.font.SysFont("arial", 20)
         note = note_font.render("Kliknij gdzie kolwiek, aby wrócić", True, (160,160,160))
         self.screen.blit(note, ((640-note.get_width())//2 ,500))
 
         pygame.display.update()
 
+        #Czekaj na kliknięcie, aby powrocic
         waiting = True
         while waiting:
             for event in pygame.event.get():
